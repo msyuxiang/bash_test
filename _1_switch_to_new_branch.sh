d4fa8a84e582d9d112d1a2b1c6b4b6eb7a06c006
yuxiang <yu@musicsecurities.com>
2017-02-27 18:20:11 +0900

    hell

--- _1_switch_to_new_branch.sh
+++ _1_switch_to_new_branch.sh
@@ -3,42 +3,46 @@
 #new branch for pull request
 echo "Please input a new branch name:"
 read branchName
-git checkout -b $branchName
-
-if [ $? -ne 0 ]; then
-	#echo "ERROE: specified branch already existed."	
-	echo "Switch to "$branchName?"(y/n)"
-	read switch_Y_N
-	case $switch_Y_N in
-		y)
-			git checkout $branchName
-			git branch
-			exit
-			;;
-		n)
-			echo "Delete "$branchName?"(y/n)"
-			read delete_Y_N
-			case $delete_Y_N in
-				y)
-					git branch -d $branchName
-					git branch
-					exit
-					;;
-				n)
-					git branch
-					exit
-					;;
-				*)
-					git branch
-					exit
-					;;
-			esac
-			;;
-		*)
-			git branch
-			exit
-			;;
-	esac
-else
-	git branch
-fi
+echo "Are you sure to create branch: "$branchName"?(y/n)"
+read create_Y_N
+case $create_Y_N in
+	y)
+		git branch $branchName	
+		echo "Do you want to switch to "$branchName"?(y/n)"
+		read switch_Y_N
+		case $switch_Y_N in
+			y)
+				git checkout $branchName
+				git branch
+				exit
+				;;
+			n)
+				echo "Delete "$branchName?"(y/n)"
+				read delete_Y_N
+				case $delete_Y_N in
+					y)
+						git branch -d $branchName
+						git branch
+						exit
+						;;
+					*)
+						git branch
+						exit
+						;;
+				esac
+				;;
+			*)
+				git branch
+				exit
+				;;
+		esac
+		;;
+	*)
+		git branch
+		exit
+		;;
+esac
+	
+#git checkout -b $branchName
+#git branch $branchName
+#git checkout $branchName
